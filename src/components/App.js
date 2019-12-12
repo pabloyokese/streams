b6f891c52d1a78a01de0113a046a16e3ebb4dc32
juan <oqundo.juan@system-in-motion.com>
2019-12-12 18:17:15 +0800

    adding routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
-const PageOne = () => {
-    return <div>PageOne
-        <Link to="/pagetwo">Navigate to page two</Link>
-    </div>
-};
-const PageTwo = () => {
-    return <div>
-        Pagetwo
-        <button>Click me!</button>
-        <Link to="/">Navigate to page one</Link>
-    </div>;
-};
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pagetwo" component={PageTwo} />
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/show" component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
