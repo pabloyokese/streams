5a183f58fa2a9fa1b61bebe4228a381b52a42a96
pabloyokese <jpo_54@hotmail.com>
2019-12-15 10:19:43 +0800

    adding finish to update

--- src/actions/index.js
+++ src/actions/index.js
@@ -28,8 +28,6 @@ export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
-  // do some programatic navigation to
-  // get the user back to the root route
   history.push('/');
 };
 
@@ -44,8 +42,9 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const deleteStream = id => async dispatch => {
