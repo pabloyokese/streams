4520506274362d0041fa2a161083546ab4f651fd
pabloyokese <jpo_54@hotmail.com>
2019-12-15 08:51:07 +0800

    adding action, dispatches and history

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -22,9 +24,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formProps) {
-    console.log(formProps);
-  }
+  onSubmit = formvalues => {
+    this.props.createStream(formvalues);
+  };
 
   render() {
     return (
@@ -55,7 +57,9 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
